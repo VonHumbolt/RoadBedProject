b441949ed578f8e1928034edada8f54e2b7713c7
VonHumbolt <kaankaplan111@gmail.com>
2023-02-05 20:58:04 +0300

    Frontend - login and register page design

--- src/main/road-bed-frontend/pages/login.js
+++ src/main/road-bed-frontend/pages/login.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import { save } from "@/redux/userSlice";
 import AuthService from "@/services/authService";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
@@ -22,66 +23,62 @@ function Login() {
   };
 
   return (
-    <div className="relative h-screen text-center flex flex-col bg-gray-100">
+    <div className="">
       <Header />
+      <div className="relative text-center flex flex-col bg-gray-100 items-center h-screen">
+        <img
+          className="opacity-70 w-full sm:max-h-96 min-h-[400px]"
+          src="https://img.freepik.com/premium-photo/lower-manhattan-from-brooklyn_155769-2248.jpg?w=1380"
+        />
 
-      <img
-        className="opacity-70 max-h-96"
-        src="https://img.freepik.com/premium-photo/lower-manhattan-from-brooklyn_155769-2248.jpg?w=1380"
-      />
-
-      <div className="absolute top-52 left-1/3 z-20 max-w-7xl">
-        <div className="bg-white z-20 p-5 rounded-2xl">
-          <div className=" h-[400px]">
-            <h3 className="text-3xl text-center font-bold text-gray-600 py-4 mb-8">
-              Login
-            </h3>
-
-            <form
-              className="space-y-4 mx-auto px-2"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div className="flex flex-col space-y-2">
-                <input
-                  {...register("email", { required: "Please enter an email." })}
-                  className="formInput mx-auto"
-                  type="email"
-                  placeholder="Email"
-                />
-                <p className="text-[#ed6172] font-semibold px-2 text-start">
-                  {errors.email?.message}
-                </p>
-                <input
-                  {...register("password", {
-                    required: "Please enter your password.",
-                  })}
-                  className="formInput mx-auto"
-                  type="password"
-                  placeholder="Password"
-                />
-                <p className="text-[#ed6172] font-semibold px-2 text-start">
-                  {errors.password?.message}
-                </p>
-              </div>
-              <p className="py-2">
-                You don't have an account?{" "}
-                <Link href="/register">
-                  <span className="font-semibold underline decoration-teal-600 cursor-pointer">
-                    Sign up now!
-                  </span>
-                </Link>
-              </p>
-              <button className="formButton" type="submit">
+        <div className="absolute top-52 mx-auto z-20 max-w-7xl">
+          <div className="bg-white z-20 pt-10 pb-10 px-16 rounded-2xl shadow-xl">
+            <div className="">
+              <h3 className="text-3xl text-center font-bold text-gray-600 py-4 mb-5">
                 Login
-              </button>
-            </form>
+              </h3>
+
+              <form className="mx-auto px-2" onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col space-y-2">
+                  <input
+                    {...register("email", {
+                      required: "Please enter an email.",
+                    })}
+                    className="formInput"
+                    type="email"
+                    placeholder="Email"
+                  />
+                  <p className="text-[#ed6172] font-semibold px-2 text-start">
+                    {errors.email?.message}
+                  </p>
+                  <input
+                    {...register("password", {
+                      required: "Please enter your password.",
+                    })}
+                    className="formInput mx-auto"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <p className="text-[#ed6172] font-semibold px-2 text-start">
+                    {errors.password?.message}
+                  </p>
+                </div>
+                <p className="pt-4 pb-1 ">You don't have an account? </p>
+                <p className="mb-2">
+                  <Link href="/register">
+                    <span className="font-semibold underline decoration-teal-600 cursor-pointer">
+                      Sign Up Now!
+                    </span>
+                  </Link>
+                </p>
+                <button className="formButton mt-6" type="submit">
+                  Login
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-      <img
-        className="opacity-70 z-0 h-max-96"
-        src="https://img.freepik.com/free-photo/elegant-circle-frame_53876-90186.jpg?w=1800&t=st=1675433461~exp=1675434061~hmac=94ae4aaa56c10b0d7ba46cbf43bfd13cac9a334b5cb2e3bdb42c8e983a084e06"
-      />
     </div>
   );
 }
